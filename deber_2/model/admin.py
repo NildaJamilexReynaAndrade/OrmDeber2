efa188bf2bc6b1326816d76f019fb68830bc8f6f
nilda reyna <nreynaa@unemi.edu.ec>
2020-08-02 16:05:43 -0500

    listo

---
+++ deber_2/model/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+
+# Register your models here.
+from model.models import *
+
+
+class ProductoAdmin(admin.ModelAdmin):
+    fields = [('descripcion','precio'),('stock', 'iva')]
+    list_display = ('descripcion','precio', 'stock','iva')
+    search_fields = ('descripcion',)
+
+class ClienteAdmin(admin.ModelAdmin):
+    fields = ['nombre','direccion','producto']
+    list_display = ('nombre','direccion')
+    search_fields = ('nombre',)
+
+class FacturaAdmin(admin.ModelAdmin):
+    fields = ['cliente','fecha','total']
+    list_display = ('cliente','fecha','total')
+    search_fields = ('cliente',)
+
+class DetalleFacturaAdmin(admin.ModelAdmin):
+    fields = ['factura','producto','cantidad','precio','subtotal']
+    list_display = ('factura','producto','cantidad','precio','subtotal')
+    search_fields = ('producto',)
+
+admin.site.register(Producto,ProductoAdmin)
+admin.site.register(Cliente,ClienteAdmin)
+admin.site.register(Factura,FacturaAdmin)
+admin.site.register(DetalleFactura,DetalleFacturaAdmin)
